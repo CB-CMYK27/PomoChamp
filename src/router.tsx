fd6c030f1bc9a6590519b59b9a398ef0196df73c
Boyle the Builder <207831690+CB-CMYK27@users.noreply.github.com>
2025-06-07 15:50:19 +0100

    Updated router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AttractScreen from './AttractScreen';
 import ModeSelect from './ModeSelect';
+import BrainDump from './components/BrainDump';
 
 export default function AppRouter() {
   return (
@@ -12,6 +13,7 @@ export default function AppRouter() {
           element={<AttractScreen onStart={() => (window.location.href = '/mode')} />}
         />
         <Route path="/mode" element={<ModeSelect />} />
+        <Route path="/brain-dump" element={<BrainDump />} />
       </Routes>
     </BrowserRouter>
   );
