6fb59fb00d7fd8b0bdda04135d6a0227c88f936c
Boyle the Builder <207831690+CB-CMYK27@users.noreply.github.com>
2025-06-08 15:58:55 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppRouter from './router';
-import QuickBattle from './components/QuickBattle'
+
 export default function App() {
   return <AppRouter />;
 }
-<Route path="/quick-battle" element={<QuickBattle />} />
