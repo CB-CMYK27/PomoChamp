47f5ccae9ef47e40dc8b56865d02d996f7a52503
Boyle the Builder <207831690+CB-CMYK27@users.noreply.github.com>
2025-06-29 17:17:27 +0100

    Updated FighterSelect.tsx

--- src/components/FighterSelect.tsx
+++ src/components/FighterSelect.tsx
@@ -133,7 +133,7 @@ export default function FighterSelect() {
                 {activeFighter.name}
               </h2>
 
-              <p className="text-center text-base truncate max-w-[16rem]">
+              <p className="italic text-center text-base whitespace-nowrap max-w-[18rem] overflow-hidden">
                 {activeFighter.quip}
               </p>
 
